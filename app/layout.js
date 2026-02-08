c6fc4d09b3671ed7afdaa5dfa81b7440dd534798
Bbester2 <bradleysim101@gmail.com>
2026-02-08 20:24:01 +0200

    Hardcode API Key fallback v1.12

--- app/layout.js
+++ app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
                     pointerEvents: 'none',
                     opacity: 0.7
                 }}>
-                    v1.11 (Fix)
+                    v1.12 (Hardcoded)
                 </div>
                 {children}
             </body>
